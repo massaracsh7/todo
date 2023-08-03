f243a6f9c8f74152bff88c685a13f9f4e25e47a1
Your Name <you@example.com>
2023-08-03 19:03:44 +0300

    feat: add header

---
+++ todo/todo/src/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <h1>
+          ToDo List
+        </h1>
+      </header>
+    </>
+  )
+}
